0ef898218181249a863925411bdb8bedc750f190
Ivan Ariel Imoff <ivanhoff.01@gmail.com>
2018-05-02 00:44:49 -0300

    Spring boot adapted to be deployed int a Tomcat and test coverage metrics

--- src/main/java/com/mercadolibre/iimoff/mutantService/MutantServiceApplication.java
+++ src/main/java/com/mercadolibre/iimoff/mutantService/MutantServiceApplication.java
@@ -2,9 +2,17 @@ package com.mercadolibre.iimoff.mutantService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
 
 @SpringBootApplication
-public class MutantServiceApplication {
+public class MutantServiceApplication extends SpringBootServletInitializer  {
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(MutantServiceApplication.class);
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(MutantServiceApplication.class, args);
